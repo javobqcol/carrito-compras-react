21c75393ab081eb6017db8cbc64c6cb84d1f56df
jaime velez <javobqcol@yahoo.es>
2023-11-24 22:43:48 -0500

    cuarto commit

--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -10,35 +10,46 @@ export const CarritoPage = () => {
   } = useContext(CarritoContext);
  
   const calcularTotal = () =>{
-    return listaCompras.reduce((total, item) => total + item.price * item.cantidad, 0).toFixed(2)
+    return listaCompras.reduce((total, item) => total + item.price * item.cantidad, 0)
   }
+  
+  const formatConfig = {
+    style: "currency",
+    currency: "COP", // CNY for Chinese Yen, EUR for Euro
+    minimumFractionDigits: 2,
+    currencyDisplay: "symbol",
+  };
+  
+  // setup formatters
+  const spanishNumberFormatter = new Intl.NumberFormat("es-CO", formatConfig);
+
 
   return (
     <div className="container padding" >
-      <table className="table">
+      <table className="table table-sm table-hover align-middle">
         <thead>
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Eliminar</th>
+          <tr className="fst-italic">
+            <th scope="col" className="text-center">Nombre</th>
+            <th scope="col" className="text-end">Precio</th>
+            <th scope="col" className="text-center">Cantidad</th>
+            <th scope="col" className="text-end">Valor.</th>
+            <th scope="col" className="text-center">Eliminar</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="table-group-divider">
           {listaCompras.map((item) => (
             <tr key={item.id}>
-              <td >{item.title}</td>
-              <td>${item.price}</td>
-              <td>
+              <td>{item.title}</td>
+              <td className="text-end">{spanishNumberFormatter.format(item.price)}</td>
+              <td className="text-center">
                 <button onClick={() => disminuirCantidad(item.id)} className="btn btn-outline-primary">-</button>
                 <button className="btn btn-primary">{item.cantidad}</button>
                 <button onClick={() => aumentarCantidad(item.id) } className="btn btn-outline-primary">+</button>
               </td>
-              <td>
-                {(item.cantidad * item.price).toFixed(2)}
+              <td className="text-end">
+               {spanishNumberFormatter.format(item.cantidad * item.price)}
               </td>
-              <td>
+              <td className="text-center">
                 <button 
                   type="button"
                   className="btn btn-danger"
@@ -48,14 +59,14 @@ export const CarritoPage = () => {
               </td>
             </tr>
           ))}
-
-          <tr>
-            <td><b><i>Total:</i></b></td>
-            <td></td>
-            <td></td>
-            <td>${calcularTotal()}</td>
-          </tr>
         </tbody>
+        <tfoot className="table-group-divider">
+          <tr>
+            <td colSpan={3} className="text-center"><b><i>Total:</i></b></td>
+            <td className="text-end">{spanishNumberFormatter.format(calcularTotal())}</td>
+            <td></td>
+          </tr>
+          </tfoot>
       </table>
       <div className="d-grid gap-2">
         <button className="btn btn-primary">Comprar</button>
